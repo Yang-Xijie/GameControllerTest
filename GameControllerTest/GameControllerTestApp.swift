804b6719c1a87bba0fe3b02526ffc7cf4300196d
yxj <564197835@qq.com>
2023-05-26 02:00:19 +0800

    Initial Commit

---
+++ GameControllerTest/GameControllerTestApp.swift
@@ -0,0 +1,12 @@
+// GameControllerTestApp.swift
+
+import SwiftUI
+
+@main
+struct GameControllerTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
